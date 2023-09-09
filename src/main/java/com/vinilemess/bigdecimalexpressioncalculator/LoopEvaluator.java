8809287e450e8fef8a8ceb27d3a0443472fdc3bc
ViniLemess <viniciuslemescaetano@gmail.com>
2023-09-09 20:14:20 -0300

    Modify loop evaluator to use while loop

--- src/main/java/com/vinilemess/bigdecimalexpressioncalculator/LoopEvaluator.java
+++ src/main/java/com/vinilemess/bigdecimalexpressioncalculator/LoopEvaluator.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class LoopEvaluator {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        for (int index = 0; index <= 100; index++) {
-            System.out.println("Informe a expressao que deseja calcular: ");
+    public static void main(final String[] args) {
+        while (true) {
+            System.out.println("Type the expression you wish to calculate:");
             String expression = scanner.nextLine();
-            System.out.println("Resultado: " + new Expression(expression).evaluate());
+            System.out.println("Result: " + new Expression(expression).evaluate());
+            System.out.println("Type 'S' to calculate another expression.");
+            String shouldContinue = scanner.nextLine();
+            if (shouldContinue.equals("S")) {
+                continue;
+            }
+            break;
         }
     }
 }
